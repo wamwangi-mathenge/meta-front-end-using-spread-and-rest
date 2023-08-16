6987effb36df35a56a587d6ab3b604976b92c11b
Brian <wamwangimathenge@gmail.com>
2023-08-16 15:55:17 +0300

    Use the spread operator to concatenate arrays

---
+++ script.js
@@ -0,0 +1,7 @@
+// Spread Operator
+// Concatenate Arrays
+const fruits = ['apple', 'pear', 'plum']
+const berries = ['blueberry', 'strawberry']
+const fruitsAndBerries = [...fruits, ...berries]
+
+console.log(fruitsAndBerries)
