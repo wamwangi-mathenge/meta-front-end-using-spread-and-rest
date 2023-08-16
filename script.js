cea3bbb29ef0f840fc7c0cf4521ab44d7ba6f195
Brian <wamwangimathenge@gmail.com>
2023-08-16 16:15:37 +0300

    Copy an object into a separate object
    This commit copies an object into a completely separate object using
    the spread operator.

--- script.js
+++ script.js
@@ -26,3 +26,13 @@ console.log("-----------------------------")
 const greeting = "Hello"
 const arrayOfChars = [...greeting]
 console.log(arrayOfChars)
+
+// Copy an object/array into a separate one
+const car1 = {
+    speed: 200,
+    color: 'yellow'
+}
+const car2 = {...car1}
+
+car1.speed = 201
+console.log(car1.speed, car2.speed)
